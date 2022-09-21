5e3bda24d2f097685280536057b3ccc452a65686
CherrySmejkal <thanassawans@gmail.com>
2022-09-21 16:01:22 +0200

    Finish ChallengeExcercise

---
+++ TSmejkal/ChallengeExcercise/script.js
@@ -0,0 +1,3 @@
+var text = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+text = text.split("$").join(" ");
+document.write(text);
