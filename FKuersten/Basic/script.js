65d22d156b85a8b9597d2d17e02bf7046393b9e1
Florian Kürsten <f.kuersten@hotmail.com>
2022-09-21 11:02:05 +0200

    Basic Exe Done - Create Inter Exe

---
+++ FKuersten/Basic/script.js
@@ -0,0 +1,6 @@
+var age = 25;
+var players = ["Martin", "Thomas", "Peter" , "Matthias" , "Niki"];
+
+console.log(`Hi my name is Martin and I am ${age} years old`);
+
+console.log(`The most valuable player of the match is ${players[2]}`);
